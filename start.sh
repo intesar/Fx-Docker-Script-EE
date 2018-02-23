0a2861f3f00731990a2d1f1f1a40952b8b51f433
Intesar Mohammed <mdshannan@gmail.com>
2018-02-23 11:46:42 -0800

    proxy compose added

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+docker node update --label-add fx-node=true NODE-ID
+# pull images on other nodes.
+
 source .env
 export $(cut -d= -f1 .env)
+
 docker stack deploy -c docker-compose-data.yaml stg1
 sleep 30
 docker stack deploy -c docker-compose-control-plane.yaml stg1
