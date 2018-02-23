5e0ccd05fdc391e56dc23183596162c35af2ee32
Intesar Mohammed <mdshannan@gmail.com>
2018-02-23 11:56:35 -0800

    add sub dir to mount point.

--- start.sh
+++ start.sh
@@ -2,6 +2,15 @@
 
 docker node update --label-add fx-node=true NODE-ID
 # pull images on other nodes.
+docker pull fxlabs/git-sync-bot
+docker pull fxlabs/control-plane
+docker pull fxlabs/bot
+docker pull fxlabs/mail-bot
+
+# create volumes -
+mkdir -p /fxcloud/elasticsearch/data
+mkdir -p /fxcloud/postgres/data
+mkdir -p /fxcloud/rabbitmq/data
 
 source .env
 export $(cut -d= -f1 .env)
