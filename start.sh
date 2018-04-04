c2e6aa4288dedde9d58873f804fd27af0003cf2f
Intesar Mohammed <mdshannan@gmail.com>
2018-04-04 12:23:52 -0700

    Stg deployment updates

--- start.sh
+++ start.sh
@@ -46,9 +46,26 @@ docker stack deploy -c docker-compose-dependents.yaml stg
 docker stack deploy -c docker-compose-proxy.yaml stg
 
 # Stg update
-docker service rm stg_fx-control-plane stg_fx-mail-bot stg_fx-vc-git-skill-bot stg_fx-it-github-skill-bot stg_fx-it-jira-skill-bot stg_fx-bot
+
+cd /opt/fx/stg/Fx-Docker-Script
+source .env
+export $(cut -d= -f1 .env)
+
+# pull images on other nodes.
+docker pull fxlabs/control-plane
+docker pull fxlabs/vc-git-skill-bot
+docker pull fxlabs/bot
+docker pull fxlabs/notification-email-skill-bot
+docker pull fxlabs/issue-tracker-github-skill-bot
+docker pull fxlabs/issue-tracker-jira-skill-bot
+docker pull fxlabs/cloud-aws-skill-bot
+docker pull fxlabs/notify-slack-skill-bot
+
+docker service rm stg_fx-control-plane stg_fx-mail-bot stg_fx-vc-git-skill-bot stg_fx-it-github-skill-bot stg_fx-it-jira-skill-bot stg_fx-bot stg_fx-cloud-aws-skill-bot stg_fx-notify-slack-skill-bot
 docker stack deploy -c docker-compose-control-plane.yaml stg
 docker stack deploy -c docker-compose-dependents.yaml stg
+
+docker restart [haproxy]
 
 # Prod update
 docker service rm prod_fx-control-plane prod_fx-bot prod_fx-git-sync-bot prod_fx-mail-bot
