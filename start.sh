cb2e5e5718d9787aab39b5e1ccc377cfe724da03
Intesar Mohammed <mdshannan@gmail.com>
2018-04-19 23:31:26 -0700

    postgres cmd

--- start.sh
+++ start.sh
@@ -83,6 +83,10 @@ tail -f /var/log/syslog | grep fx
 tail -f /var/log/syslog | grep bot
 tail -f /var/log/syslog | grep control
 
+# postgres
+docker exec -it <postgres>
+psql -U fx_admin fx
+
 
 # Prod update
 docker service rm prod_fx-control-plane prod_fx-mail-bot prod_fx-vc-git-skill-bot prod_fx-it-github-skill-bot prod_fx-it-jira-skill-bot prod_fx-bot prod_fx-cloud-aws-skill-bot prod_fx-notification-slack-skill-bot
