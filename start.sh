865658e6084b9d05b638e0f706cc02e5ecaebcd2
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-07-13 11:44:11 +0530

    uat tag update to uat1

--- start.sh
+++ start.sh
@@ -125,11 +125,11 @@ docker pull fxlabs/issue-tracker-jira-skill-bot
 docker pull fxlabs/cloud-aws-skill-bot
 docker pull fxlabs/notification-slack-skill-bot
 
-docker service rm uat_fx-control-plane
-docker stack deploy -c docker-compose-control-plane.yaml uat
+docker service rm uat1_fx-control-plane
+docker stack deploy -c docker-compose-control-plane.yaml uat1
 
-docker service rm uat_fx-mail-bot uat_fx-vc-git-skill-bot uat_fx-it-github-skill-bot uats_fx-it-jira-skill-bot uat_fx-cloud-aws-skill-bot uat_fx-notification-slack-skill-bot
-docker stack deploy -c docker-compose-dependents.yaml uat
+docker service rm uat1_fx-mail-bot uat1_fx-vc-git-skill-bot uat1_fx-it-github-skill-bot uats1_fx-it-jira-skill-bot uat1_fx-cloud-aws-skill-bot uat1_fx-notification-slack-skill-bot
+docker stack deploy -c docker-compose-dependents.yaml uat1
 
 docker restart [haproxy]
 
