b5c3b9eaa31f2279b927dd4ec91fc7d1672a5877
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-26 19:45:10 +0530

    fxissue instruction included for uat

--- start.sh
+++ start.sh
@@ -121,6 +121,7 @@ docker pull fxlabs/vc-git-skill-bot
 docker pull fxlabs/bot
 docker pull fxlabs/notification-email-skill-bot
 docker pull fxlabs/issue-tracker-github-skill-bot
+docker pull fxlabs/issue-tracker-fx-skill-bot
 docker pull fxlabs/issue-tracker-jira-skill-bot
 docker pull fxlabs/cloud-aws-skill-bot
 docker pull fxlabs/notification-slack-skill-bot
@@ -128,7 +129,7 @@ docker pull fxlabs/notification-slack-skill-bot
 docker service rm uat1_fx-control-plane
 docker stack deploy -c docker-compose-control-plane.yaml uat1
 
-docker service rm uat1_fx-mail-bot uat1_fx-vc-git-skill-bot uat1_fx-it-github-skill-bot uat1_fx-it-jira-skill-bot uat1_fx-cloud-aws-skill-bot uat1_fx-notification-slack-skill-bot
+docker service rm uat1_fx-mail-bot uat1_fx-vc-git-skill-bot uat1_fx-it-github-skill-bot uat1_fx-it-fx-skill-bot uat1_fx-it-jira-skill-bot uat1_fx-cloud-aws-skill-bot uat1_fx-notification-slack-skill-bot
 docker stack deploy -c docker-compose-dependents.yaml uat1
 
 docker restart [haproxy]
