5e57e0155871cd2ff56211cd0790de8cc5029585
Mohammed S Ali <shoukathmd@gmail.com>
2018-09-27 21:20:56 +0530

    fx issues added to prod instructions

--- start.sh
+++ start.sh
@@ -172,6 +172,7 @@ docker pull fxlabs/vc-git-skill-bot
 docker pull fxlabs/bot
 docker pull fxlabs/notification-email-skill-bot
 docker pull fxlabs/issue-tracker-github-skill-bot
+docker pull fxlabs/issue-tracker-fx-skill-bot
 docker pull fxlabs/issue-tracker-jira-skill-bot
 docker pull fxlabs/cloud-aws-skill-bot
 docker pull fxlabs/notification-slack-skill-bot
@@ -179,7 +180,7 @@ docker pull fxlabs/notification-slack-skill-bot
 docker service rm prod_fx-control-plane
 docker stack deploy -c docker-compose-control-plane.yaml prod
 
-docker service rm prod_fx-mail-bot prod_fx-vc-git-skill-bot prod_fx-it-github-skill-bot prod_fx-it-jira-skill-bot prod_fx-cloud-aws-skill-bot prod_fx-notification-slack-skill-bot
+docker service rm prod_fx-mail-bot prod_fx-vc-git-skill-bot prod_fx-it-fx-skill-bot prod_fx-it-github-skill-bot prod_fx-it-jira-skill-bot prod_fx-cloud-aws-skill-bot prod_fx-notification-slack-skill-bot
 docker service rm prod_fx-cloud-aws-skill-bot
 docker stack deploy -c docker-compose-dependents.yaml prod
 
